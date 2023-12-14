3bdf66f33b687a24f074a305b44c191e8636c3fe
Yujjio <wuk9@rpi.edu>
2023-12-14 17:29:07 -0500

    comment

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,12 +1,12 @@
 return require('packer').startup(function()
     use 'wbthomason/packer.nvim'  -- packer can manage itself
-    use 'Abstract-IDE/Abstract-cs'
-    use 'preservim/nerdtree'
-    use 'github/copilot.vim'
-    use 'vim-airline/vim-airline'
-    use 'vim-airline/vim-airline-themes'
-    use 'Yggdroot/indentLine'
-    use 'mhinz/vim-startify'
-    use 'airblade/vim-gitgutter'
-    use 'tpope/vim-fugitive'
+    use 'Abstract-IDE/Abstract-cs'  -- color theme
+    use 'preservim/nerdtree'  -- file explorer
+    use 'github/copilot.vim'  -- github copilot
+    use 'vim-airline/vim-airline'  -- status bar
+    use 'vim-airline/vim-airline-themes'  -- status bar themes
+    use 'Yggdroot/indentLine'  -- indent lines
+    use 'mhinz/vim-startify'  -- start screen
+    use 'airblade/vim-gitgutter'  -- git diff in gutter
+    use 'tpope/vim-fugitive'  -- git commands
 end)
